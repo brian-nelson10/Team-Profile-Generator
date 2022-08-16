9f956867e90b42f92b3b424c905acf9abd1e1560
brian <bn3l10@gmail.com>
2022-08-16 17:13:45 -0400

    finished input from index

--- index.js
+++ index.js
@@ -8,23 +8,58 @@
 //add last div and body html closing tags
 
 //add member(); startHtml(); addHtml(); .then(function); finsihHtml();
-
+const {writeFile, copyFile} = require('./dist/generate-site.js');
 const inquirer = require("inquirer");
+const generateHtml = require('./utils/generateHtml.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
 const Manager = require('./lib/Manager.js');
+
 const fs = require('fs');
 
-function runGen() {
-    addTeamMember();
-    renderHtml();
-}
+// function runGen() {
+//     teamName();
+//     addTeamMember();
+//     renderHtmlBase();
+// }
 
-function addTeamMember() {
-    inquirer
+const teamName = () => {
+    console.log(`
+    ====================
+    Start a Team Profile
+    ====================
+    `);
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'team',
+                message: 'Enter your Team Profile Name',
+                validate: teamInput => {
+                    if (teamInput) {
+                        return true;
+                    } else {
+                        console.log('Enter Team Profile Name!!');
+                        return false;
+                    }
+                }
+            }
+        ])};
+const addTeamMember = teamData => {
+    console.log(`
+    ==================
+    Add a Team Member!
+    ==================
+    `);
+    
+      // If there is no 'team' array property, create one
+      if (!teamData.team) {
+        teamData.team = [];
+      }
+      return inquirer
         .prompt([
         {
-            type: 'text',
+            type: 'input',
             name: 'name',
             message: 'What is the team members name?',
             validate: nameInput => {
@@ -50,7 +85,7 @@ function addTeamMember() {
             }
         },
         {
-            type: 'email',
+            type: 'input',
             name: 'email',
             message: 'Enter Team Members Email Address',
             validate: emailInput => {
@@ -62,15 +97,86 @@ function addTeamMember() {
             }
         },
         {
-            type: 'checkbox',
+            type: 'list',
             name: 'role',
             message: 'Please select Team Members Role',
             choices: ['Manager', 'Engineer', 'Intern']
         }
     ])
-   
-runGen()
-//.then(inputData => {
-    console.log(inputData);
-//    return generateHtml(inputData);
-//});
+    .then(function({name, id, email, role}) {
+        let roleInput = "";
+        if (role === "Manager") {
+            roleInput = "Office Phone Number";
+        } else if (role === "Engineer") {
+            roleInput = "GitHub Username";
+        } else {
+            roleInput = "School Name";
+        }
+
+        inquirer
+            .prompt([{
+                name: 'roleInput',
+                message: `Enter Team Members ${roleInput}`
+            },
+            {
+                type: 'confirm',
+                name: 'addMember',
+                message: 'Would you like to add more Team Members?',
+                default: false            
+            }])
+            .then(profileData => {
+                //teamData.team.push(profileData);
+                if (profileData.addMember) {
+                    return addTeamMember(teamData);
+                } else {
+                    return teamData;
+                }
+            });
+
+            // .then(function({roleInput}) {
+            //     let newMember;
+            //     if (role === "Manager") {
+            //         newMember = new Manager(name, id, email, roleInput);
+            //     } else if (role === "Engineer") {
+            //         newMember = new Engineer(name,id,email,roleInput);
+            //     } else {
+            //         newMember = new Intern(name,id,email,roleInput);
+            //     }
+//             teamData.team.push(newMember);
+//             renderHtml()
+
+//             .then(function() {
+// //                teamData.team.push(profileData);
+//                 if (addMember === "YES") {
+//                     return addTeamMember(teamData);
+//                 } else {
+// //                    return renderCloseHtml(),
+//                     console.log('Html has been generated');
+//                 }
+//             });
+        });
+    }
+//    );
+//}
+
+
+//call to run app
+teamName()
+.then(addTeamMember)
+.then(teamData => {
+    console.log(teamData);
+  //  return generateHtml(teamData);
+})
+// .then(pageHTML => {
+//     return writeFile(pageHTML);
+// })
+// // .then(writeFileResponse => {
+// //     console.log(writeFileResponse);
+// //     return copyFile();
+// })
+// .then(copyFileResponse => {
+//     console.log(copyFileResponse);
+//   })
+.catch(err => {
+    console.log(err);
+});
--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -1,8 +1,29 @@
-const fs = require('fs');
-const inquirer = require("inquirer");
+//const fs = require('fs');
+//const inquirer = require("inquirer");
 
 
+// const renderHtml = () => {
+//     const html = `
+//     <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>${data.team}</title>
+// </head>
+// <body>
+//     <header>
 
+//     </header>
+
+// <main>
+//     ${renderHtml(newMember)}
+// </main>
+// </body>
+// </html>
+//     `
+// };
 
 
 
